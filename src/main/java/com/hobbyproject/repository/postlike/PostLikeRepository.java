75fa0b700035d3859fc1d58f058ccba83195c4e3
junyoung <adad568@naver.com>
2025-01-13 21:13:04 +0900

    Feat(PostLikeRepository) : PostLikeRepository 추가

---
+++ src/main/java/com/hobbyproject/repository/postlike/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package com.hobbyproject.repository.postlike;
+
+import com.hobbyproject.entity.Member;
+import com.hobbyproject.entity.Post;
+import com.hobbyproject.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
+    PostLike findByMemberAndPost(Member member, Post post);
+
+    Long countAllByPost(Post post);
+}
