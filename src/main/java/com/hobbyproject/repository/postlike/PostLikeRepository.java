5cd820eb6aa362b4aecd77489b20b1528df9a285
junyoung <adad568@naver.com>
2025-02-05 20:49:10 +0900

    Refactor(PostLikeRepository): existsByPostAndMember 메서드를 existsByPost_PostIdAndMember_LoginId로 ID 기반 조회로 수정

--- src/main/java/com/hobbyproject/repository/postlike/PostLikeRepository.java
+++ src/main/java/com/hobbyproject/repository/postlike/PostLikeRepository.java
@@ -1,14 +1,9 @@
 package com.hobbyproject.repository.postlike;
 
-import com.hobbyproject.entity.Member;
-import com.hobbyproject.entity.Post;
 import com.hobbyproject.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
-    PostLike findByMemberAndPost(Member member, Post post);
 
-    Long countAllByPost(Post post);
-
-    boolean existsByPostAndMember(Post post, Member member);
+    boolean existsByPost_PostIdAndMember_LoginId(Long postId, String memberName);
 }
