0707287fb7704eeaca31a7ef4eb23d787ee47a72
junyoung <adad568@naver.com>
2025-02-11 21:33:18 +0900

    Feat(MemberRepository): loginId를 통해서 이름 반환 메서드 추가

--- src/main/java/com/hobbyproject/repository/member/MemberRepository.java
+++ src/main/java/com/hobbyproject/repository/member/MemberRepository.java
@@ -2,10 +2,11 @@ package com.hobbyproject.repository.member;
 
 import com.hobbyproject.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Locale;
-import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByLoginId(String loginId);
+public interface MemberRepository extends JpaRepository<Member, String> {
+    @Query("SELECT m.name FROM Member m WHERE m.loginId = :id")
+    String findMemberName(@Param("id") String loginId);
 }
