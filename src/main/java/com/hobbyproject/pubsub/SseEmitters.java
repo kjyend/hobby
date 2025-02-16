2bda58be02c5da06868c22576e790dc967c7773a
junyoung <adad568@naver.com>
2025-02-16 20:57:38 +0900

    Feat(SseEmitter): SSE 이용한 알림 기능 추가

---
+++ src/main/java/com/hobbyproject/pubsub/SseEmitters.java
@@ -0,0 +1,36 @@
+package com.hobbyproject.pubsub;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SseEmitters {
+    private final Map<String, SseEmitter> emitters=new ConcurrentHashMap<>();
+
+    public SseEmitter addEmitter(String userId) {
+        SseEmitter emitter = new SseEmitter(30 * 60 * 1000L);
+        emitters.put(userId,emitter);
+        emitter.onCompletion(()->emitters.remove(userId));
+        emitter.onTimeout(()->emitters.remove(userId));
+        return emitter;
+    }
+
+    public void removeEmitter(String userId) {
+        emitters.remove(userId);
+    }
+
+    public void sendNotification(String userId, String message){
+        SseEmitter emitter = emitters.get(userId);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event().name("notification").data(message));
+            } catch (IOException e) {
+                emitters.remove(userId);
+            }
+        }
+    }
+}
