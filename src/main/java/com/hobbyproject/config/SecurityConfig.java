d010afd0d3bfd5c2c549d395056ae4723b05b21e
junyoung <adad568@naver.com>
2025-03-12 22:27:06 +0900

    Refactor(SecurityConfig): Session 에서 Jwt 관련 SecurityFilterChain 변경

--- src/main/java/com/hobbyproject/config/SecurityConfig.java
+++ src/main/java/com/hobbyproject/config/SecurityConfig.java
@@ -1,44 +1,52 @@
 package com.hobbyproject.config;
 
+import com.hobbyproject.jwt.JwtAuthFilter;
+import com.hobbyproject.jwt.JwtUtil;
+import com.hobbyproject.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@ComponentScan(basePackages = "com.hobbyproject")
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
+    private final JwtAuthFilter jwtAuthFilter;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtUtil jwtUtil) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtUtil = jwtUtil;
+        this.jwtAuthFilter = new JwtAuthFilter(customUserDetailsService,jwtUtil);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sessionManager->sessionManager.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/login").permitAll()
                         .requestMatchers("/post/write","/post/edit/*","/post/*").authenticated()
                         .anyRequest().permitAll())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/",true)
-                        .failureUrl("/login?error=true")
-                        .permitAll())
-                .logout(logout->logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout=true")
-                        .invalidateHttpSession(true)
-                        .permitAll())
-                .sessionManagement(session->session
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(false))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
