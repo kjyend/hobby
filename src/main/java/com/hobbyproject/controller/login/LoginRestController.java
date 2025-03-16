678bde84e2fd7ad316f9cf3be707e59b9076c12b
junyoung <adad568@naver.com>
2025-03-16 20:52:08 +0900

    Feat(LoginRestController): Jwt 로그인 검증 메서드 추가

--- src/main/java/com/hobbyproject/controller/login/LoginRestController.java
+++ src/main/java/com/hobbyproject/controller/login/LoginRestController.java
@@ -2,22 +2,26 @@ package com.hobbyproject.controller.login;
 
 import com.hobbyproject.dto.member.request.LoginDto;
 import com.hobbyproject.dto.member.request.SignupDto;
+import com.hobbyproject.jwt.JwtUtil;
 import com.hobbyproject.service.login.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequiredArgsConstructor
 public class LoginRestController {
 
     private final LoginService loginService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody SignupDto signupDto, BindingResult bindingResult){
@@ -38,9 +42,36 @@ public class LoginRestController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody LoginDto loginDto, BindingResult bindingResult){
         String token = loginService.login(loginDto);
         return ResponseEntity.status(HttpStatus.OK).body(token);
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<Boolean> loginCheckBar(HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+        boolean isLoggedIn = false;
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            if(jwtUtil.validateToken(token)) {
+                isLoggedIn = true;
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(isLoggedIn);
+    }
+
+    @GetMapping("/login/user-info")
+    public ResponseEntity<Map<String, Object>> getUserInfo(@RequestHeader("Authorization") String token) {
+        Map<String, Object> response = new ConcurrentHashMap<>();
+        if(token != null && token.startsWith("Bearer ")) {
+            String userId = jwtUtil.getLoginId(token.replace("Bearer ", ""));
+            response.put("isLoggedIn", true);
+            response.put("userId", userId);
+            return ResponseEntity.ok(response);
+        }
+        response.put("isLoggedIn", false);
+        return ResponseEntity.ok().body(response);
     }
 }
