2a9abea5d35f6df8b02d85db9ec0e30fa4a84c1c
junyoung <adad568@naver.com>
2025-03-16 20:56:20 +0900

    Refactor(HomeController): 불필요한 코드 제거

--- src/main/java/com/hobbyproject/controller/HomeController.java
+++ src/main/java/com/hobbyproject/controller/HomeController.java
@@ -1,22 +1,13 @@
 package com.hobbyproject.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-
-        boolean isLoggedIn = userDetails != null;
-
-        model.addAttribute("isLoggedIn", isLoggedIn);
-
+    public String home() {
         return "home";
-
     }
 }
