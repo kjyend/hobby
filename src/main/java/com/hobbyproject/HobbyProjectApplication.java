cf640b1d9172e314a88da195d980c8496f42a1aa
junyoung <adad568@naver.com>
2025-04-09 20:43:44 +0900

    Feat(Consumer, Producer, Config): rabbitMQ 메시지 큐 관련 코드 생성

--- src/main/java/com/hobbyproject/HobbyProjectApplication.java
+++ src/main/java/com/hobbyproject/HobbyProjectApplication.java
@@ -1,5 +1,6 @@
 package com.hobbyproject;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableRabbit
 public class HobbyProjectApplication {
 
     public static void main(String[] args) {
